affca10eb633ecd119c36197c1bfb4ed62f0e50f
Siequnu <11821606+Siequnu@users.noreply.github.com>
2018-04-30 17:47:01 +0800

    Connectivity: Add verbose logging messages with time

--- connectivity.py
+++ connectivity.py
@@ -2,16 +2,26 @@ import subprocess
 from subprocess import call, PIPE, STDOUT
 import shlex
 import time
+import sys
+import arrow
 
+# Global variables
+LAST_VPN_RESTART = False
+
+# Get the return code of an externally run process
 def get_return_code (cmd, stderr=STDOUT):
 	args = shlex.split(cmd)
 	return call(args, stdout=PIPE, stderr=stderr)
 	
+# Run a ping check and return exit code
 def check_connectivity ():
 	cmd = 'ping -c 1 8.8.8.8'
 	return get_return_code(cmd) == 0
 
-def main ():
+# Main entrance to program
+def main (sleep_time_secs = 20):
+	global LAST_VPN_RESTART
+	
 	while True:
 		if check_connectivity () == False:
 			print ('Internet is down... killing any existing OpenVPN process.')
@@ -26,8 +36,18 @@ def main ():
 							 '/etc/openvpn/auth.txt'])
 			print (str(proc))
 			print ('OpenVPN restarted successfully!')
+			LAST_VPN_RESTART = arrow.now()
 		else:
 			print ('Internet is up.')
-		time.sleep (20)
+			if LAST_VPN_RESTART:
+				print ('Last VPN restart was ' + str(LAST_VPN_RESTART.humanize()) + ' , at: ' + str(LAST_VPN_RESTART))
+		time.sleep (sleep_time_secs)
 
-main ()
+# Get sleep time argument, if present in arguments
+print ('Starting connectivity manager...')
+if len(sys.argv) > 1:
+	sleep_time_secs = sys.argv[1]
+	print ('Setting sleep time as ' + str(sleep_time_secs))
+	main (int(sleep_time_secs))
+else:
+	main()
